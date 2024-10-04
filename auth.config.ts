d23ac73519ca9ec4ee8b93ffd8d5ff6aa93068b2
Diego Díaz <diefco.web@gmail.com>
2024-10-04 11:21:18 -0500

    🎉 feat(auth.config): add credentials provider for authentication

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,8 @@
 // import GitHub from "@auth/core/providers/github";
 import { defineConfig } from "auth-astro";
+import { db, eq, User } from "astro:db";
+import Credentials from "@auth/core/providers/credentials";
+import bcrypt from "bcryptjs";
 
 export default defineConfig({
   providers: [
@@ -8,5 +11,30 @@ export default defineConfig({
     //   clientId: import.meta.env.GITHUB_CLIENT_ID,
     //   clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
     // }),
+
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async ({ email, password }) => {
+        const [user] = await db
+          .select()
+          .from(User)
+          .where(eq(User.email, `${email}`));
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        if (!bcrypt.compareSync(`${password}`, user.password)) {
+          throw new Error("Invalid password");
+        }
+
+        const { password: _, ...rest } = user;
+
+        return rest;
+      },
+    }),
   ],
 });
