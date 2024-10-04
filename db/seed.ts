f93f1fb6999de2b578ef1a206326e90c3ba6fd44
Diego Díaz <diefco.web@gmail.com>
2024-10-03 20:03:33 -0500

    🎉 feat (seed): implement seed function to populate roles and users in seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,30 @@
-import { db } from 'astro:db';
+import { db, Role, User } from "astro:db";
+import { v4 as UUID } from "uuid";
+import bcrypt from "bcryptjs";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  const roles = [
+    { id: "admin", name: "Admin" },
+    { id: "user", name: "User" },
+  ];
+
+  const jhonDoe = {
+    id: UUID(),
+    name: "Jhon Doe",
+    email: "jhon.doe@google.com",
+    password: bcrypt.hashSync("123456"),
+    role: "admin",
+  };
+
+  const janeDoe = {
+    id: UUID(),
+    name: "Jane Doe",
+    email: "jane.doe@google.com",
+    password: bcrypt.hashSync("123456"),
+    role: "user",
+  };
+
+  await db.insert(Role).values(roles);
+  await db.insert(User).values([jhonDoe, janeDoe]);
 }
